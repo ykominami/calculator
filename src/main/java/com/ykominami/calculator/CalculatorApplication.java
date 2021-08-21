c18954c9d55d07020f28927eb787e7a134984358
yasuo kominami <ykominami@gmail.com>
2021-08-22 01:39:55 +0900

    fix

--- src/main/java/com/ykominami/calculator/CalculatorApplication.java
+++ src/main/java/com/ykominami/calculator/CalculatorApplication.java
@@ -3,6 +3,7 @@ package com.ykominami.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CalculatorApplication {
 
